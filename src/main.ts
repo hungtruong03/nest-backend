ba77618f33ebd5b886cb4c6231f19e115d3ee3f4
21120079 <hungtruonghl11@gmail.com>
2024-10-29 22:16:42 +0700

    test

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,9 +11,11 @@ async function bootstrap() {
   //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   //   credentials: true,
   // });
-  app.enableCors({
+  app.use(cors({
     origin: '*',
-  });
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
