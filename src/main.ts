4d294a636b9f4d923fa88fc5400441f70944779d
21120079 <hungtruonghl11@gmail.com>
2024-10-29 22:14:10 +0700

    test

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  // app.enableCors({
+  //   origin: 'http://localhost:5173',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   credentials: true,
+  // });
   app.enableCors({
-    origin: 'http://localhost:5173',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+    origin: '*',
   });
   await app.listen(process.env.PORT ?? 3000);
 }
